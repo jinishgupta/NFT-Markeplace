6be95d2ab1c51653a83a746d426507c4a3c980c8
jinishgupta <jinishgupta272@gmail.co>
2024-12-18 13:39:58 +0530

    making changes

---
+++ main_page.js
@@ -0,0 +1,9 @@
+function scrolll() {
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(350, 0)
+}
+
+function scrollr() {
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(-350, 0)
+}
