6a7d5f90d88ecc74c2d39b2bbc15faf973704df7
jinishgupta <jinishgupta272@gmail.co>
2024-12-19 16:11:36 +0530

    main page designed

--- ART NFT/main_page.js
+++
@@ -1,9 +0,0 @@
-function scrolll() {
-    var left = document.querySelector(".scroll-images");
-    left.scrollBy(350, 0)
-}
-
-function scrollr() {
-    var right = document.querySelector(".scroll-images");
-    right.scrollBy(-350, 0)
-}
