96a25f5671e3a3172e8ad7106596e6209ef4a7c2
jinishgupta <jinishgupta272@gmail.co>
2024-12-20 11:36:11 +0530

    logout option added and firebase authentication completed

---
+++ logout.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+import {getAuth, signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFj5ZjLfmG18d6uawRrESmR0iadVek-H4",
+  authDomain: "nft-marketplace-9a468.firebaseapp.com",
+  projectId: "nft-marketplace-9a468",
+  storageBucket: "nft-marketplace-9a468.firebasestorage.app",
+  messagingSenderId: "208333584068",
+  appId: "1:208333584068:web:b0bd814d46324504d4fb91",
+  measurementId: "G-5JRN00H2YD"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+console.log(app);
+
+   //----- Logout code start	  
+   document.getElementById("logout").addEventListener("click", function() {
+       signOut(auth).then(() => {
+           // Sign-out successful.
+           console.log('Log-out successful.');
+           window.location.href="login.html";
+         }).catch((error) => {
+           // An error happened.
+           console.log('An error happened.',error);
+         });		  		  
+   });
